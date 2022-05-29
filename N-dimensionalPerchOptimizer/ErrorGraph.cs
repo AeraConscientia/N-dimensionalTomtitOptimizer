91dc3947883e2a50f5beaaf39fc1eeb8b890280f
AeraConscientia <annacom84kolessa@yandex.ru>
2022-05-30 01:27:04 +0300

    small bugs fixed
    График ошибки исправлен

--- N-dimensionalPerchOptimizer/ErrorGraph.cs
+++ N-dimensionalPerchOptimizer/ErrorGraph.cs
@@ -17,15 +17,6 @@ namespace N_dimensionalTomtitOptimizer
             InitializeComponent();
             Result result = Result.GetInstance();
 
-            //switch (TaskNumber)
-            //{
-            //    case 6:
-            //        N_dim = N_dim / 2;
-            //        break;
-            //    default:
-            //        break;
-            //}
-
             CleanErrorGraphics();
 
             UpdateErrorGraph(TaskNumber, N_dim);
@@ -41,18 +32,6 @@ namespace N_dimensionalTomtitOptimizer
             switch (TaskNumber)
             {
                 case 6:
-                    N_dim = N_dim / 2;
-                    break;
-                default:
-                    break;
-            }
-
-            switch (TaskNumber)
-            {
-                case 6:
-                    //for (int i = 0; i < N_dim; i++)
-                    //    chartI_Error.Series[0].Points.AddXY(i, );
-
                     double[] AnaliticU = new double[N_dim];
                     for (int i = 0; i < N_dim; i++)
                     {
@@ -69,7 +48,6 @@ namespace N_dimensionalTomtitOptimizer
 
         private void CleanErrorGraphics()
         {
-            //chartI_Error.Series[0].Points.Clear();
             chartU_Error.Series[0].Points.Clear(); 
         }
             
